cc168d6c0ede0e3eab151bb2a69f2c4e898f7059
Jesse Kriss <jesse@jklabs.net>
2018-01-01 23:19:02 -0800

    Keeping binaries out of the repo
    Former-commit-id: 528088b735b2238d107e856f7c59d2bd2a6e0e3a

--- download.sh
+++ download.sh
@@ -1,3 +1,7 @@
+mkdir -p linux-armv7
+mkdir -p linux-x64
+mkdir -p win
+mkdir -p macos
 curl https://altcloud.io/linux-armv7/altcloud > linux-armv7/altcloud
 curl https://altcloud.io/linux-x64/altcloud > linux-x64/altcloud
 curl https://altcloud.io/macos/altcloud > macos/altcloud
