37c19f66853da464da94614ea26c146a71182f7b
Jesse Kriss <jesse@jklabs.net>
2018-01-01 23:16:03 -0800

    Updating binaries
    Former-commit-id: 4e8a6e4d4bae9355d209de3586949772883e6b42

---
+++ download.sh
@@ -0,0 +1,4 @@
+curl https://altcloud.io/linux-armv7/altcloud > linux-armv7/altcloud
+curl https://altcloud.io/linux-x64/altcloud > linux-x64/altcloud
+curl https://altcloud.io/macos/altcloud > macos/altcloud
+curl https://altcloud.io/win/altcloud > win/altcloud.exe
